f9f408f1cc86a9eca1d8ad8c3a18b47a2e940d2e
Julio Pedro López Bayón <juliolb@educastur.org>
2025-09-30 17:00:58 +0200

    Actualización

---
+++ src/main/java/unidad1/Ejercicio8.java
@@ -0,0 +1,21 @@
+package unidad1;
+
+/*
+ * Escribe un programa que calcule el perímetro y el área de un círculo
+ * a partir de su radio (r) usando las expresiones siguientes:
+ * 
+ * 				perímetro = 2𝜋𝑟
+ *              área = 𝜋𝑟2
+ * 
+ * Para especificar el valor de  usa la expresión Math.PI.
+ * 
+ */
+
+public class Ejercicio8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
