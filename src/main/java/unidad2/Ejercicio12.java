bc97d71d57bd8a9bdd79143c9da45e500a225b62
Julio Pedro López Bayón <juliolb@educastur.org>
2025-10-08 20:56:37 +0200

    Actualización

---
+++ src/main/java/unidad2/Ejercicio12.java
@@ -0,0 +1,32 @@
+package unidad2;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+ * Escribe un programa que genere de forma aleatoria números enteros
+ * pertenecientes al intervalo [100, 999] y los vaya sumando hasta que
+ * se supere un valor previamente introducido por teclado.
+ * 
+ * Antes de finalizar, el programa mostrará el resultado de la suma y
+ * la cantidad de valores acumulados.
+ * 
+ */
+
+public class Ejercicio12 {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		int suma = 0;
+		int contador = 0;
+		Scanner in = new Scanner(System.in);
+		System.out.print("Valor máximo de la suma: ");
+		int max = in.nextInt();
+		while (suma < max) {
+			int n = random.nextInt(899) + 100;
+			suma += n;
+			contador++;
+		}
+	}
+
+}
