0e64d5a72d83b51fbea2a09973b91bb124447721
Julio Pedro López Bayón <juliolb@educastur.org>
2025-10-16 21:39:23 +0200

    Actualización

--- src/main/java/unidad2/Ejercicio17.java
+++ src/main/java/unidad2/Ejercicio17.java
@@ -27,36 +27,37 @@ public class Ejercicio17 {
 		Scanner in = new Scanner(System.in);
 		Random random = new Random();
 		
-//		int n = random.nextInt(99001) + 1000;
-//		int cpu = random.nextInt(n - 1 + 1) + 1;
+		int max = random.nextInt(99001) + 1000;
+		int cpu = random.nextInt(max) + 1;
+		int contador = 0;
 		
-//		System.out.printf("He pensado un número entre 1 y %d, adivina cuál es:", cpu);
-//		int jugador;
-//		do {
-//			System.out.print("Número: ");
-//			jugador = in.nextInt();
-//			if (cpu != jugador) {
-//				System.out.print("Has fallado, el número es ");
-//				if (cpu < jugador)
-//					System.out.println("menor.");
-//				else
-//					System.out.println("mayor.");
-//			}
-//		} while(cpu == jugador);
-//		System.out.println("Has acertado!");
-		
-		int cpu = random.nextInt(random.nextInt(99001) + 1000) + 1;
-		
-		System.out.printf("He pensado un número entre 1 y %d, adivina cuál es:", cpu);
+		System.out.printf("He pensado un número entre 1 y %d, adivina cuál es:\\n", max);
 		int jugador;
 		do {
+			contador++;
 			System.out.print("Número: ");
 			jugador = in.nextInt();
-			if (cpu != jugador)
-				System.out.printf("Has fallado, el número es %s",
-						cpu < jugador ? "menor" : "mayor");
-		} while(cpu == jugador);
-		System.out.println("Has acertado!");
+			if (cpu != jugador) {
+				System.out.print("Has fallado, el número es ");
+				if (cpu < jugador)
+					System.out.println("menor.");
+				else
+					System.out.println("mayor.");
+			}
+		} while(cpu != jugador);
+		System.out.printf("Has acertado después de %d intentos!", contador);
+		
+//		System.out.printf("He pensado un número entre 1 y %d, adivina cuál es:\n", max);
+//		int jugador;
+//		do {
+//			contador++;
+//			System.out.print("Número: ");
+//			jugador = in.nextInt();
+//			if (cpu != jugador)
+//				System.out.printf("Has fallado, el número es %s\n",
+//						cpu < jugador ? "menor" : "mayor");
+//		} while(!(cpu == jugador));
+//		System.out.printf("Has acertado después de %d intentos!", contador);
 	}
 
 }
