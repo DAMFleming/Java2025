50b8ab3b28799ec9955260377d9505bc9e3f3e6f
Julio Pedro López Bayón <juliolb@educastur.org>
2025-11-25 17:16:08 +0100

    Actualización

--- src/main/java/unidad3/arrays/MetodosOrdenacion.java
+++ src/main/java/unidad3/arrays/MetodosOrdenacion.java
@@ -20,28 +20,82 @@ public class MetodosOrdenacion {
 	}
 		
 	static void seleccionDirecta(long [] v) {
-		
+		int aux_j;
+		long min;
+		for(int i=0; i<v.length-1; i++) {
+			aux_j=i;
+			for(int j=i;j<v.length; j++)
+				if(v[j]<v[aux_j]) 
+					aux_j=j;
+				
+			if(aux_j!=i) {
+				min= v[aux_j];
+				v[aux_j]= v[i];
+				v[i]= min;
+			}
+			
+		}
 	}
 
 	static void intercambioDirecto(long [] v) {
-		
+		for(int indice = 0; indice < v.length - 1; indice++) {
+			for(int b = v.length - 1; b > indice; b--) {
+				if (v[b] < v[b -1]) {
+					long g = v[b];
+					v[b] = v[b - 1];
+					v[b - 1] = g;
+				}
+			}
+		}
 	}
 	
 	
 	public static void main(String[] args) {
-		long [] v = {44, 55, 12, 42, 94, 18, 6, 67};
-		insercionDirecta(v);
-		System.out.println(Arrays.toString(v));
+//		long [] v = {44, 55, 12, 42, 94, 18, 6, 67};
+//		intercambioDirecto(v);
+//		System.out.println(Arrays.toString(v));
 		
 		Random r = new Random();
-		long [] v2 = new long[1000000];
-		for (int i=0; i<v2.length; i++)
-			v2[i] = r.nextLong();
-		System.out.println("Ordenando");
-		long t0 = System.nanoTime();
-//		insercionDirecta(v2);
-		Arrays.sort(v2);
-		long t1 = System.nanoTime();
+		long [] v = new long[100000];
+		for (int i=0; i<v.length; i++)
+			v[i] = r.nextLong();
+		
+		long [] vaux;
+		long t0, t1;
+		vaux = Arrays.copyOf(v, 100000);
+		System.out.println("inserción directa");
+		t0 = System.nanoTime();
+		insercionDirecta(vaux);
+		t1 = System.nanoTime();
 		System.out.println(t1 - t0);
+		
+		vaux = Arrays.copyOf(v, 100000);
+		System.out.println("selección directa");
+		t0 = System.nanoTime();
+		seleccionDirecta(vaux);
+		t1 = System.nanoTime();
+		System.out.println(t1 - t0);
+		
+		vaux = Arrays.copyOf(v, 100000);
+		System.out.println("intercambio directo");
+		t0 = System.nanoTime();
+		intercambioDirecto(vaux);
+		t1 = System.nanoTime();
+		System.out.println(t1 - t0);
+		
+		vaux = Arrays.copyOf(v, 100000);
+		System.out.println("Arrays.sort");
+		t0 = System.nanoTime();
+		Arrays.sort(vaux);
+		t1 = System.nanoTime();
+		System.out.println(t1 - t0);
+		
+		vaux = Arrays.copyOf(v, 100000);
+		System.out.println("Arrays.parallelSort");
+		t0 = System.nanoTime();
+		Arrays.parallelSort(vaux);
+		t1 = System.nanoTime();
+		System.out.println(t1 - t0);
+		
 	}
 }
