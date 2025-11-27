6694b6d5502e58246f2e2d66a495f60f6e1165a3
Julio Pedro López Bayón <juliolb@educastur.org>
2025-11-27 21:42:07 +0100

    Actualización

---
+++ src/main/java/unidad3/arrays/Ejercici11.java
@@ -0,0 +1,23 @@
+package unidad3.arrays;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Ejercici11 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[][] secuencias = new int[n][];
+		for (int i = 0; i < n; i++) {
+			int m = sc.nextInt();
+			secuencias[i] = new int[m];
+			for (int j = 0; j < m; j++) {
+				secuencias[i][j] = sc.nextInt();
+			}
+		}
+	}
+
+}
