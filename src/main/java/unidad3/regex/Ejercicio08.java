8c6c8fe3b3f3e50f8243a03f54ae79987cc39b8c
Julio Pedro López Bayón <juliolb@educastur.org>
2025-12-01 18:33:00 +0100

    Actualización

--- src/main/java/unidad3/regex/Ejercicio08.java
+++ src/main/java/unidad3/regex/Ejercicio08.java
@@ -1,6 +1,7 @@
 package unidad3.regex;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -16,19 +17,20 @@ import java.util.stream.Collectors;
 
 public class Ejercicio08 {
 
-	public static void main(String[] args) {
-		BufferedReader in = new BufferedReader(
+	public static void main(String[] args) throws IOException {
+		try (BufferedReader in = new BufferedReader(
 				new InputStreamReader(
-						Ejercicio08.class.getResourceAsStream("/documento.txt")));
-		String documento = in.lines().collect(Collectors.joining("\n"));
-		System.out.println(documento);
-		Pattern p = Pattern.compile("<uppercase>(.*)</uppercase>");
-		Matcher m = p.matcher(documento);
-		StringBuffer sb = new StringBuffer();
-		while (m.find())
-			m.appendReplacement(sb, m.group(1).toUpperCase());
-		m.appendTail(sb).toString();
-		System.err.println(sb.toString());
+						Ejercicio08.class.getResourceAsStream("/documento.txt")))) {
+			String documento = in.lines().collect(Collectors.joining("\n"));
+			System.out.println(documento);
+			Pattern p = Pattern.compile("<uppercase>(.*)</uppercase>");
+			Matcher m = p.matcher(documento);
+			StringBuffer sb = new StringBuffer();
+			while (m.find())
+				m.appendReplacement(sb, m.group(1).toUpperCase());
+			m.appendTail(sb).toString();
+			System.err.println(sb.toString());
+		}
 	}
 
 }
